3af118fe822d2c2f0664b64ca9cb5f66b107a361
olegizt <olegdubna@mail.ru>
2023-12-20 22:59:15 +0300

    init_rep

---
+++ nfsc_script.sh
@@ -0,0 +1,15 @@
+yum install nfs-utils wget
+systemctl enable firewalld --now
+echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
+systemctl daemon-reload
+systemctl restart remote-fs.target
+wget -P /home/vagrant/ https://raw.githubusercontent.com/olegizt/DZ6_NFS/main/c_check.sh
+chmod +x /home/vagrant/c_check.sh
+
+
+echo '#!/bin/bash' >> /usr/local/sbin/motd.sh
+chmod +x /usr/local/sbin/motd.sh
+echo 'printf "\e[0;37;42mПривет! Проверка настроек ДЗ по NFS на клиенте. Запустите скрипт с_check.sh\e[0m\n"' >> /usr/local/sbin/motd.sh
+echo "PrintMotd no" >> /etc/ssh/sshd_config
+systemctl restart sshd
+echo '/usr/local/sbin/motd.sh' >> /etc/profile
---
+++ nfss_script.sh
@@ -0,0 +1,22 @@
+yum install nfs-utils wget
+systemctl enable firewalld --now
+firewall-cmd --add-service="nfs3" --add-service="rpc-bind" --add-service="mountd" --permanent 
+firewall-cmd --reload
+systemctl enable nfs --now
+wget -P /home/vagrant/ https://raw.githubusercontent.com/olegizt/DZ6_NFS/main/s_check.sh
+chmod +x /home/vagrant/s_check.sh
+mkdir -p /srv/share/upload
+chown -R nfsnobody:nfsnobody /srv/share
+chmod 0777 /srv/share/upload
+cat << EOF > /etc/exports
+/srv/share 192.168.50.11/32(rw,sync,root_squash)
+EOF
+exportfs -r
+
+
+echo '#!/bin/bash' >> /usr/local/sbin/motd.sh
+chmod +x /usr/local/sbin/motd.sh
+echo 'printf "\e[0;37;42mПривет! Проверка настроек ДЗ по NFS на сервере. Запустите скрипт s_check.sh\e[0m\n"' >> /usr/local/sbin/motd.sh
+echo "PrintMotd no" >> /etc/ssh/sshd_config
+systemctl restart sshd
+echo '/usr/local/sbin/motd.sh' >> /etc/profile
